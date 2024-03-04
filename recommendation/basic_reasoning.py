b3160a7e7b71e506770bec7352c9713ff0f1a589
Hugo Plottu <hugo.plottu@student-cs.fr>
2024-03-04 08:46:52 +0100

    changements mineurs

--- recommendation/basic_reasoning.py
+++ recommendation/basic_reasoning.py
@@ -82,7 +82,6 @@ def recommendation(onto, user_id):
     # sort the films by the number of times they appear
     sorted_films = sorted(score.items(), key=lambda item: item[1], reverse=True)
 
-    #afficher le nom des films déjà vus
     return sorted_films
     
 
